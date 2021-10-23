85ffb46ca675872130eae22506c125e599f01317
gocreating <gocreating@gmail.com>
2021-10-23 22:23:01 +0800

    Create an API to search an user by fullname

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from urllib.parse import parse_qs
+
 from django.http import JsonResponse
 from django.urls import path
 
@@ -15,6 +17,19 @@ def handle_users(request):
         users = request.session.query(User).all()
         return JsonResponse([user.to_dict() for user in users], safe=False)
 
+@managed_transaction
+def search_user(request):
+    if request.method == 'GET':
+        query_string = request.GET.urlencode()
+        query = parse_qs(query_string)
+        filtered_user = request.session.query(User)\
+            .filter(User.fullname == query['fullname'][0])\
+            .first()
+        if filtered_user is None:
+            return JsonResponse(None, safe=False)
+        return JsonResponse(filtered_user.to_dict(), safe=False)
+
 urlpatterns = [
     path('users/', handle_users),
+    path('users/search/', search_user),
 ]
